c642a52f9fc0fe37967b80dec0a8aacab5bd9493
droger <david.roger@dashlane.com>
2025-04-22 14:09:34 +0200

    -

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ var distance = {
   dice: "1d6",
   entries: [
     { number: '1', element: 'Close' },
-    { number: '2-3', element: 'Near' },
+    { number: '2-3', element: 'Near at #1d3 meter' },
     { number: '4-6', element: 'Far' },],
 }
 
@@ -27,7 +27,7 @@ var roomType = {
 
 var dice = {
   sides: 6,
-  roll: function() {
+  roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
   }
@@ -71,13 +71,22 @@ function rollOnTable(table) {
 }
 
 function write(content) {
+  const content = "blah blah #1d3 blah blah"
   var placeHolder = document.getElementById('placeholder');
+  if (content.includes("#")) {
+    var before = content.split("#")[0];
+    var after = content.split("#")[1];
+    var expression = after.split(" ")[0];
+    var rest = after.split(" ")[1];
+    var rollResult = roll(expression);
+    console.log(`${before} ${rollResult} ${rest}`);
+  }
   placeHolder.innerHTML = content
 }
 
 var button = document.getElementById('button');
 
-button.onclick = function() {
+button.onclick = function () {
   var result = rollOnTable(roomType);
   console.log("roll result:" + result);
   write(result);
